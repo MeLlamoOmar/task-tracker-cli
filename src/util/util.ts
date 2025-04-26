e48b4759a80b90e6c6fd515b75171e9200ee7c3d
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-04-26 14:18:20 -0400

    Implement createTask function and adjust getTasksList to return full TaskJson

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,20 @@
 import { readFile } from "node:fs/promises";
+// import { randomUUID } from "node:crypto";
 
 import Task, { TaskJson, TaskStatus } from "../types/Task";
+
+export const createTask = async (taskName: string): Promise<Task> => {
+  const taskList = await getTasksList();
+
+  const newTask: Task = {
+    id: taskList.tasks.length + 1,
+    description: taskName,
+    status: 'todo',
+    createdAt: new Date(Date.now())
+  };
+
+  return newTask;
+}
 
 export const createCheckbox = (status: TaskStatus): string | undefined => {
   switch (status) {
@@ -18,12 +32,13 @@ export const createCheckbox = (status: TaskStatus): string | undefined => {
 export const getTasksList = async (): Promise<TaskJson> => {
   let jsonData: TaskJson
   const rawData = await readFile('output.json', {encoding: 'utf-8'});
+
   if (rawData.length === 0) {
     jsonData = {
       tasks: []
     }
   } else {
-    jsonData = JSON.parse(rawData).tasks;
+    jsonData = JSON.parse(rawData);
   }
 
   return jsonData;
