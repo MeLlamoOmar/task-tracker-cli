07dd0faed8bb889f3b71445dbba908dd0c6436f4
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-04-26 14:18:47 -0400

    Fix dev script command to correctly list tasks and ensure task addition is awaited

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import process from "node:process";
 
 import { argsOptions } from "./types/const.d";
-import { createTask, addTask } from "./controller/taskController";
+import { addTask, listTasks } from "./controller/taskController";
+import { createTask } from "./util/util";
+
 import Task from "./types/Task";
 
 const main = async () => {
 	const args = process.argv.slice(2);
 	const command = args[0];
-	console.log("Command:", command);
 
 	switch (command) {
 		case argsOptions.ADD:
-			console.log("Adding a task...");
-			const newTask: Task = createTask(args[1]);
+			console.log("Adding a task...\n");
+			const newTask: Task = await createTask(args[1]);
 			await addTask(newTask);
 			console.log(`Task added suscessuccessfully (ID: ${newTask.id})`);
 			break;
@@ -23,7 +24,8 @@ const main = async () => {
 			console.log("Updating a task...");
 			break;
 		case argsOptions.LIST:
-			console.log("Listing tasks...");
+			console.log("Listing tasks: \n");
+			await listTasks()
 			break;
 		case argsOptions.MARK:
 			console.log("Marking a task...");
