6fea9b1f581fa1fe2c11c34a7c7d7db3a18b1efa
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-04-26 14:18:37 -0400

    Refactor addTask and listTasks to utilize getTasksList for task management

--- src/controller/taskController.ts
+++ src/controller/taskController.ts
@@ -1,46 +1,19 @@
 import { randomUUID } from 'node:crypto';
 import fs from 'fs/promises';
-import Task from '../types/Task';
-import { createCheckbox } from '../util/util';
-
-export const createTask = (taskName: string): Task => {
-  const newTask: Task = {
-    id: randomUUID(),
-    description: taskName,
-    status: 'todo',
-    createdAt: new Date(Date.now())
-  };
-
-  return newTask;
-}
+import Task, { TaskJson } from '../types/Task';
+import { createCheckbox, getTasksList } from '../util/util';
 
 export const addTask = async (newTodo: Task) => {
-  let jsonData
-  const rawData = await fs.readFile('output.json', {encoding: 'utf-8'});
-  if (rawData.length === 0) {
-    jsonData = {
-      tasks: [newTodo]
-    }
-  } else {
-    jsonData = JSON.parse(rawData);
-    jsonData.tasks = [...jsonData.tasks, newTodo];
-  }
+  let taskList: TaskJson = await getTasksList();
+  taskList.tasks = [...taskList.tasks, newTodo]
 
-  await fs.writeFile('output.json', JSON.stringify(jsonData), {encoding: 'utf-8'});
+  await fs.writeFile('output.json', JSON.stringify(taskList), {encoding: 'utf-8'});
 }
 
 export const listTasks = async () => {
-  let jsonData
-  const rawData = await fs.readFile('output.json', {encoding: 'utf-8'});
-  if (rawData.length === 0) {
-    jsonData = {
-      tasks: []
-    }
-  } else {
-    jsonData = JSON.parse(rawData);
-  }
+  const taskList: TaskJson = await getTasksList()
 
-  jsonData.task.forEach((task: Task, index: number) => {
+  taskList.tasks.forEach((task: Task, index: number) => {
     const checkBox = createCheckbox(task.status);
     console.log(`${index + 1}) ${checkBox ? checkBox : 'error'} ${task.description}`)
   });
